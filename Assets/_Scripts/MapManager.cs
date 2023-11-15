aa63e5584c2e1bbb37e1ceb3978c822797bc9d4a
Cinniven <Mu8988@gmail.com>
2023-11-15 12:50:29 +0100

    MapManager

---
+++ Assets/_Scripts/MapManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapManager : MonoBehaviour
+{
+    [SerializeField] GameObject prefab;
+    [SerializeField] Vector2 spawnPoint, size;
+    [SerializeField] Vector3 rotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnPlatform(prefab, spawnPoint, size, rotation);
+        SpawnPlatform(prefab, -spawnPoint, size, -rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SpawnPlatform(GameObject _prefab, Vector2 _spawnPoint, Vector2 _size, Vector3 _rotation)
+    {
+        GameObject _platform = Instantiate(_prefab, _spawnPoint, Quaternion.identity, this.gameObject.transform);
+        _platform.transform.localScale += new Vector3(_size.x, _size.y, 0);
+        _platform.transform.rotation = Quaternion.Euler(0, 0, _rotation.z);
+    }
+}
